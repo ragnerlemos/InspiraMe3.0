fa74676593e691e10b88abfd1c9da5340b623daa
Ragner Lemos <efeitosbd@gmail.com>
2025-09-02 21:59:19 +0000

    Na tela dos modelos tem como eles ficarem menor ? Um do lado do outro, d

--- src/app/modelos/page.tsx
+++ src/app/modelos/page.tsx
@@ -26,7 +26,7 @@ export default function ModelosPage() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
         {templates.map((template) => {
           const editorUrl = new URLSearchParams();
           editorUrl.set('templateId', template.id.toString());
