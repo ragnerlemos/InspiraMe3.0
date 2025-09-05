754b9e6c3083264d8cf51dad7f9fa8c6eabaaf86
Ragner Lemos <efeitosbd@gmail.com>
2025-09-05 13:49:14 +0000

    entao vamos colocar a nossa area de vizualizaão  alinhada superior

--- src/app/editor-de-video/components/cliente-editor.tsx
+++ src/app/editor-de-video/components/cliente-editor.tsx
@@ -285,7 +285,7 @@ export function EditorClient() {
   return (
     <div className="flex flex-col md:flex-row w-full h-full overflow-hidden">
       {/* Área de visualização */}
-      <div className="md:h-full w-full h-[calc(100%-4rem)] flex justify-center items-center bg-muted/40 p-4 md:p-8">
+      <div className="w-full h-[calc(100%-4rem)] md:h-full flex justify-center items-start bg-muted/40 p-4 md:p-8">
         <VisualizacaoEditor
             aspectRatio={currentState.aspectRatio}
             backgroundStyle={currentState.backgroundStyle}
