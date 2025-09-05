a8b41ce4b5e0fc1f4811de43be1ada013887d4f6
Ragner Lemos <efeitosbd@gmail.com>
2025-09-05 15:00:52 +0000

    nao era pra mecher na versao mobile

--- src/app/editor-de-video/components/cliente-editor.tsx
+++ src/app/editor-de-video/components/cliente-editor.tsx
@@ -285,7 +285,7 @@ export function EditorClient() {
   return (
     <div className="flex flex-col md:flex-row w-full h-full">
       {/* Área de visualização */}
-      <div className="flex-1 flex flex-col items-center justify-start bg-muted/40 p-4 md:p-8 md:h-[calc(100vh-8rem)]">
+      <div className="flex-1 flex flex-col items-center justify-start bg-muted/40 p-4 md:p-8 md:max-h-[calc(100vh-8rem)]">
         <VisualizacaoEditor
             aspectRatio={currentState.aspectRatio}
             backgroundStyle={currentState.backgroundStyle}
@@ -380,6 +380,8 @@ logoPositionY={currentState.logoPositionY}
     
 
 
+
+    
 
     
 
