b974c501458a21eb03278c5d1797186e4c895eee
Ragner Lemos <efeitosbd@gmail.com>
2025-09-06 05:18:15 +0000

    agora ficou exagerado estoutrando tem que ser de uma forma q nao estoura

--- src/app/editor-de-video/components/visualizacao.tsx
+++ src/app/editor-de-video/components/visualizacao.tsx
@@ -151,12 +151,12 @@ export function VisualizacaoEditor({
   return (
     <div
       id="editor-preview"
-      className="w-full max-h-full flex items-center justify-center"
+      className="w-full h-full flex items-center justify-center"
     >
       <div
         id="editor-preview-content"
         className={cn(
-          "relative overflow-hidden shadow-2xl @container bg-black w-full rounded-lg",
+          "relative overflow-hidden shadow-2xl @container bg-black w-full h-full rounded-lg",
           {
             "aspect-square": aspectRatio === "1:1",
             "aspect-[9/16]": aspectRatio === "9:16",
