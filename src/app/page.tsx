88407114b8143b8af96b7553743b5dc2358f8a53
Ragner Lemos <efeitosbd@gmail.com>
2025-09-06 20:29:58 +0000

    isso ta funciionando mas a nossa,  esta usando barra de rolagem?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -48,7 +48,7 @@ export default function HomePage() {
   }, [toast]);
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col">
       <main className="flex-1">
         <div className="container mx-auto py-8 px-4">
           {/* Cabeçalho da página com título e descrição. */}
