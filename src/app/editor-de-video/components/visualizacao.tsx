5005ffb57882f7838e77129328723935f909dd74
Ragner Lemos <efeitosbd@gmail.com>
2025-09-06 05:35:04 +0000

    nao reverteu

--- src/app/editor-de-video/components/visualizacao.tsx
+++ src/app/editor-de-video/components/visualizacao.tsx
@@ -151,12 +151,12 @@ export function VisualizacaoEditor({
   return (
     <div
       id="editor-preview"
-      className="flex items-center justify-center"
+      className="flex items-center justify-center w-full"
     >
       <div
         id="editor-preview-content"
         className={cn(
-          "relative overflow-hidden shadow-2xl @container bg-black rounded-lg max-w-full",
+          "relative overflow-hidden shadow-2xl @container bg-black rounded-lg w-full",
           {
             "aspect-square": aspectRatio === "1:1",
             "aspect-[9/16]": aspectRatio === "9:16",
