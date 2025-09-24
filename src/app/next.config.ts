1a5452036780d5bfa08642ded606fcaa6548ed04
Ragner Lemos <efeitosbd@gmail.com>
2025-09-24 14:44:31 +0000

    1️⃣ Remover exportação estática
    No next.config.js, garanta exatamente i

---
+++ src/app/next.config.ts
@@ -0,0 +1,33 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    serverActions: true,
+  },
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https' as const,
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
--- src/lib/dados.ts
+++ src/lib/dados.ts
@@ -1,4 +1,5 @@
 
+"use server";
 
 import { google } from 'googleapis';
 
