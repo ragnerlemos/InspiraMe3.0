f533c7dd7d88753ee07d6c3fadb5270d0dfcc1f4
Ragner Lemos <efeitosbd@gmail.com>
2025-10-23 17:25:24 +0000

    run build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 
 "use client";
 
+import { usePathname } from 'next/navigation';
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from '@/components/theme-provider';
 import './globals.css';
-import { AppLayout } from './app-layout';
+import { AppHeader } from './cabecalho-app';
+import { useGoogleFonts } from "@/hooks/use-google-fonts";
+import { EditorProvider } from './editor-de-video/contexts/editor-context';
+import { FirebaseClientProvider } from "@/firebase";
 
 // Layout raiz da aplicação que envolve todas as páginas.
 export default function RootLayout({
@@ -12,6 +16,13 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
+  // Carrega e injeta as fontes do Google para evitar problemas de CORS
+  useGoogleFonts();
+
+  const pathname = usePathname();
+  const isEditorPage = pathname.startsWith('/editor-de-video');
+
   return (
     <html lang="pt-BR" suppressHydrationWarning className="h-full">
       <head>
@@ -24,11 +35,17 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-            {/* O Toaster é usado para exibir notificações no aplicativo. */}
-            <Toaster />
-            <AppLayout>
-                {children}
-            </AppLayout>
+          <FirebaseClientProvider>
+            <EditorProvider>
+              <Toaster />
+              <div className="flex flex-col h-full">
+                  {!isEditorPage && <AppHeader />}
+                  <div className="flex-1 flex flex-col min-h-0">
+                      {children}
+                  </div>
+              </div>
+            </EditorProvider>
+          </FirebaseClientProvider>
         </ThemeProvider>
       </body>
     </html>
