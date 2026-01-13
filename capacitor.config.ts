5f9be83852e6648d98f4de25c96bc169fcf59843
Ragner Lemos <efeitosbd@gmail.com>
2026-01-13 05:06:21 +0000

    Missing Gradle project configuration folder: .settings [{ 	"resource": "

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,42 +7,6 @@ const config: CapacitorConfig = {
   server: {
     androidScheme: 'https',
   },
-  plugins: {
-    Firebase: {
-      config: {
-        projectId: "quotevid2-57726828-e0133",
-        appId: "1:523999002605:web:ccb32fab01f90f09ebb1e4",
-        apiKey: "AIzaSyCJ22veYzQeQ5wGCumfVX5Kh_B3IDD-WxY",
-        authDomain: "quotevid2-57726828-e0133.firebaseapp.com",
-        measurementId: "G-XWNJDR3QHX",
-        messagingSenderId: "523999002605",
-      },
-    },
-    CapacitorAndroid: {
-      fullscreen: true,
-      overlay: false,
-      permissions: [
-        {
-          alias: 'publicStorage',
-          name: 'READ_EXTERNAL_STORAGE',
-        },
-        {
-          alias: 'publicStorage',
-          name: 'WRITE_EXTERNAL_STORAGE',
-        },
-        {
-          alias: 'camera',
-          name: 'CAMERA',
-        },
-      ]
-    }
-  },
-  android: {
-    buildOptions: {
-      compileSdkVersion: 34,
-      targetSdkVersion: 34
-    }
-  }
 };
 
 export default config;
