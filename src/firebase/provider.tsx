1e3b52e1d95550ce477dfb34e00d368c03fe725f
Ragner Lemos <efeitosbd@gmail.com>
2026-01-18 04:53:09 +0000

    BUILD FAILED in 46s studio-159551456:~/studio{restaurada_versao_2026-01-

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -4,14 +4,6 @@ import React, { DependencyList, createContext, useContext, ReactNode, useMemo } 
 import { FirebaseApp } from 'firebase/app';
 import { Firestore } from 'firebase/firestore';
 import { Auth } from 'firebase/auth';
-import { FirebaseErrorListener } from '@/components/FirebaseErrorListener'
-
-interface FirebaseProviderProps {
-  children: ReactNode;
-  firebaseApp: FirebaseApp;
-  firestore: Firestore;
-  auth: Auth;
-}
 
 // Combined state for the Firebase context
 export interface FirebaseContextState {
@@ -54,7 +46,6 @@ export const FirebaseProvider: React.FC<FirebaseProviderProps> = ({
 
   return (
     <FirebaseContext.Provider value={contextValue}>
-      <FirebaseErrorListener />
       {children}
     </FirebaseContext.Provider>
   );
