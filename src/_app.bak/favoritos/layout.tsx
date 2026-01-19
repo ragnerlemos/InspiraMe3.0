310e8cd87d7552d3abc2b2e77e14629739213767
Ragner Lemos <efeitosbd@gmail.com>
2026-01-19 17:24:34 +0000

    https://studio.firebase.google.com/studio-159551456    sem per esta pare

--- src/_app.bak/favoritos/layout.tsx
+++
@@ -1,8 +0,0 @@
-
-import { ReactNode } from "react";
-import MainAppLayout from "../(main)/layout";
-
-// Este layout envolve a página de favoritos e aplica o layout padrão do aplicativo.
-export default function FavoritosLayout({ children }: { children: ReactNode }) {
-    return <MainAppLayout>{children}</MainAppLayout>;
-}
