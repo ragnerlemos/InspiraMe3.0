d733296bc922201c2ccbeb7ced424d7d1ac7d44e
Ragner Lemos <efeitosbd@gmail.com>
2026-01-19 17:39:53 +0000

    ERROR: JAVA_HOME is not set and no 'java' command could be found in your

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'standalone',
   reactStrictMode: true,
   typescript: {
     ignoreBuildErrors: true,
@@ -9,7 +9,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
